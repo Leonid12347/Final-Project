16f4a196c52f121378e10912a36fe480597b54b6
Leonid12347 <124260342+Leonid12347@users.noreply.github.com>
2023-04-03 17:00:25 +0300

    commit two pages

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -41,4 +41,40 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: '.swiper-pagination',
   },
-});s
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 2.5,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: false,
+    loopFillGroupWithBlank: true,
+    breakpoints: {
+        1000: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+
+        768: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        484: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        },
+        280: {
+            slidesPerView: 1.2,
+            spaceBetween: 20,
+        },
+        100: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
+
+    navigation: {
+        nextEl: "#swiper-button-next-2",
+        prevEl: "#swiper-button-prev-2",
+    },
+});
